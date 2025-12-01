0b1282a42a736ea4e807b78126f02ebde32736f8
Erestion <erestion@gmail.com>
2025-12-01 13:23:21 +0200

    minor fix

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+// ...
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // <--- ПОВЕРНИ ТУТ ПРОСТО СЛЕШ
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4000',
-        changeOrigin: true,
-      }
-    }
-  }
+  base: '/teammate-finder/',
+  // ...
 })
